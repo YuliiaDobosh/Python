3ad5c8575a53495c932f2e78d29736701aebdc6a
YuliiaDobosh <yuliia.dobosh.it.2021.lpnu.ua>
2023-09-20 19:50:17 +0300

    change calculation task 5

--- lab 1/calc.py
+++ lab 1/calc.py
@@ -1,6 +1,6 @@
 import math
 
-class Calculator:
+class Calculator: #Завд №1 
     # Ініціалізуємо атрибути, які будуть використовуватися для операцій калькулятора
     def __init__(self): #Завд №2
         self.num1 = None
@@ -10,12 +10,12 @@ class Calculator:
     def get_user_input(self):
         # Запитуємо користувача про введення чисел і оператора
         try:
-            self.num1 = float(input("Введіть перше число: "))
+            self.num1 = float(input("Введіть перше число: ")) # Завд №3 введення 2-ох значеннь та оператор
             self.operator = input("Введіть операцію (+, -, *, /, ^, √, %): ") 
             if self.operator not in ('+', '-', '*', '/', '^', '√', '%'): # Завд №4 + №9
                 print("Помилка: Невідомий оператор.")
                 return
-            if self.operator in ('^', '√', '%'):
+            if self.operator in ('+', '-', '*', '/', '^', '√', '%'):
                 if self.operator != '√':
                     self.num2 = float(input("Введіть друге число: "))
                 else:
@@ -23,29 +23,34 @@ class Calculator:
         except ValueError:
             print("Помилка: Введіть коректні числа.")
 
-    def calculate(self): #Завд №5
+    def calculate(self): # Завд №5
         try:
-            if self.operator == '+':
-                result = self.num1 + self.num2
-            elif self.operator == '-':
-                result = self.num1 - self.num2
-            elif self.operator == '*':
-                result = self.num1 * self.num2
-            elif self.operator == '/':
-                if self.num2 != 0: #Завд №6
-                    result = self.num1 / self.num2
-                else:
-                    print("Помилка: Ділення на нуль.")
-                    return
-            elif self.operator == '^':
-                result = self.num1 ** self.num2
-            elif self.operator == '√':
-                result = math.sqrt(self.num1)
-            elif self.operator == '%':
-                result = self.num1 % self.num2
-            else:
-                print("Помилка: Невідомий оператор.")
-                return
+            result = None
+            if self.operator is not None:
+                num1, num2 = self.num1, self.num2 if self.num2 is not None else 0
+
+                match self.operator:
+                    case '+':
+                        result = num1 + num2
+                    case '-':
+                        result = num1 - num2
+                    case '*':
+                        result = num1 * num2
+                    case '/':
+                        if num2 != 0: #Завд №6
+                            result = num1 / num2
+                        else:
+                            print("Помилка: Ділення на нуль.")
+                            return
+                    case '^':
+                        result = num1 ** num2
+                    case '√':
+                        result = math.sqrt(num1)
+                    case '%':
+                        result = num1 % num2
+                    case _:
+                        print("Помилка: Невідомий оператор.")
+                        return
 
             print(f"Результат: {self.num1} {self.operator} {self.num2 if self.num2 is not None else ''} = {result:.2f}")
         except Exception as e:
