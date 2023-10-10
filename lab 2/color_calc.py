c275fd43a06ccdd286625dcf3a436b6b2c0986f3
YuliiaDobosh <yuliia.dobosh.it.2021.lpnu.ua>
2023-10-10 22:49:45 +0300

    add color_calc

---
+++ lab 2/color_calc.py
@@ -0,0 +1,6 @@
+from calc import Calculator 
+from termcolor import *
+class ColorCalculator (Calculator):
+    def display_result(self):
+        print(colored(f"Результат: {self._result}", "red")) #інкапсуляція , що ми можемо в класах нащадків використовувати захищені зміні 
+ 
--- lab 2/main.py
+++ lab 2/main.py
@@ -1,6 +1,7 @@
 from program import Program
 from calc import Calculator
+from color_calc import ColorCalculator
 
 if __name__ == "__main__":
-    calculator: Program = Calculator()
+    calculator: Program = ColorCalculator()
     calculator.run()
