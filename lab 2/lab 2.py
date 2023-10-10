69d492e5ace4e17b5292788dae172b5b0daac554
YuliiaDobosh <yuliia.dobosh.it.2021.lpnu.ua>
2023-10-10 22:18:28 +0300

    lab 2

--- lab 2/lab 2.py
+++ lab 2/lab 2.py
@@ -1,28 +1,52 @@
 import math
 
-class Calculator:
+class Program: 
+    def run(self): ##поліморфізм ми його перевизначаємо 
+        pass
+
+class Calculator(Program ): ##наслідування
     def __init__(self):
-        self.result = 0
+        self._result = 0 ##інкапсуляція поля класу є захищеними protected
 
     def input_numbers(self):
         try:
-            self.num1 = float(input("Введіть перше число: "))
-            self.num2 = float(input("Введіть друге число: "))
+            self._num1 = float(input("Введіть перше число: "))
+            self._num2 = float(input("Введіть друге число: "))
         except ValueError:
             print("Помилка: Введені дані мають бути числовими.")
             self.input_numbers()
 
     def input_operator(self):
-        self.operator = input("Введіть операцію (+, -, *, /, ^, sq, %): ")
-        if self.operator not in ['+', '-', '*', '/', '^', 'sq', '%']:
+        self._operator = input("Введіть операцію (+, -, *, /, ^, sq, %): ")
+        if self._operator not in ['+', '-', '*', '/', '^', 'sq', '%']:
             print("Помилка: Недійсна операція.")
             self.input_operator()
 
     def calculate(self):
-        pass
+        
+        if self._operator == '+':
+            self._result = self._num1 + self._num2
+        elif self._operator == '-':
+            self._result = self._num1 - self._num2
+        elif self._operator == '*':
+            self._result = self._num1 * self._num2
+        elif self._operator == '/':
+            if self._num2 == 0:
+                print("Помилка: Ділення на нуль.")
+            else:
+                self._result = self._num1 / self._num2
+        elif self._operator == '^':
+            self._result = self._num1 ** self._num2
+        elif self._operator == 'sq':
+            if self._num1 < 0:
+                print("Помилка: Корінь від'ємного числа.")
+            else:
+                self._result = math.sqrt(self._num1)
+        elif self._operator == '%':
+            self._result = self._num1 % self._num2
 
     def display_result(self):
-        print(f"Результат: {self.result}")
+        print(f"Результат: {self._result}")
 
     def run(self):
         while True:
@@ -34,29 +58,6 @@ class Calculator:
             if choice.lower() != 'так':
                 break
 
-class BasicCalculator(Calculator):
-    def calculate(self):
-        if self.operator == '+':
-            self.result = self.num1 + self.num2
-        elif self.operator == '-':
-            self.result = self.num1 - self.num2
-        elif self.operator == '*':
-            self.result = self.num1 * self.num2
-        elif self.operator == '/':
-            if self.num2 == 0:
-                print("Помилка: Ділення на нуль.")
-            else:
-                self.result = self.num1 / self.num2
-        elif self.operator == '^':
-            self.result = self.num1 ** self.num2
-        elif self.operator == 'sq':
-            if self.num1 < 0:
-                print("Помилка: Корінь від'ємного числа.")
-            else:
-                self.result = math.sqrt(self.num1)
-        elif self.operator == '%':
-            self.result = self.num1 % self.num2
-
 if __name__ == "__main__":
-    calculator = BasicCalculator()
+    calculator: Program = Calculator()
     calculator.run()
